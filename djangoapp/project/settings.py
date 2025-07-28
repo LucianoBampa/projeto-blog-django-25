a5973c988bc397ad908d6576a88b79cc266639bf
LucianoBampa <lucbam2@gmail.com>
2025-07-28 15:11:49 -0300

    Usando Context Processors para injetar valores em todos os templates do Django

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -61,9 +61,12 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'site_setup.context_processors.context_processor_example',
+                'site_setup.context_processors.site_setup',
             ],
         },
     },
