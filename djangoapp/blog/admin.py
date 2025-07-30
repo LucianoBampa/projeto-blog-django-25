3a5564eef5658a2e3aadb4ccea954c721d575a7f
LucianoBampa <lucbam2@gmail.com>
2025-07-30 12:50:18 -0300

    Configurando o django-summernote como editor HTML em TextField nos models do Django

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,14 +1,15 @@
 from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = 'id', 'name', 'slug',
-    list_display_links = 'name',
-    search_fields = 'id', 'name', 'slug',
+    list_display = ('id', 'name', 'slug',)
+    list_display_links = ('name',)
+    search_fields = ('id', 'name', 'slug',)
     list_per_page = 10
-    ordering = '-id',
+    ordering = ('-id',)
     prepopulated_fields = {
         "slug": ('name',),
     }
@@ -16,44 +17,46 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = 'id', 'name', 'slug',
-    list_display_links = 'name',
-    search_fields = 'id', 'name', 'slug',
+    list_display = ('id', 'name', 'slug',)
+    list_display_links = ('name',)
+    search_fields = ('id', 'name', 'slug',)
     list_per_page = 10
-    ordering = '-id',
+    ordering = ('-id',)
     prepopulated_fields = {
         "slug": ('name',),
     }
 
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'is_published',
-    list_display_links = 'title',
-    search_fields = 'id', 'title', 'slug',
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = ('id', 'title', 'is_published',)
+    list_display_links = ('title',)
+    search_fields = ('id', 'title', 'slug',)
     list_per_page = 50
-    list_filter = 'is_published',
-    list_editable = 'is_published',
-    ordering = '-id',
+    list_filter = ('is_published',)
+    list_editable = ('is_published',)
+    ordering = ('-id',)
     prepopulated_fields = {
         "slug": ('title',),
     }
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'is_published',  'created_by',
-    list_display_links = 'title',
-    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = ('id', 'title', 'is_published',  'created_by',)
+    list_display_links = ('title',)
+    search_fields = ('id', 'slug', 'title', 'excerpt', 'content',)
     list_per_page = 50
-    list_filter = 'category', 'is_published'
-    list_editable = 'is_published',
-    ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    list_filter = ('category', 'is_published')
+    list_editable = ('is_published',)
+    ordering = ('-id',)
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by',)
     prepopulated_fields = {
         "slug": ('title',),
     }
-    autocomplete_fields = 'tags', 'category',
+    autocomplete_fields = ('tags', 'category',)
 
     def save_model(self, request, obj, form, change):
         if change:
